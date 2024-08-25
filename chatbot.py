a7a250164a07dc6657c68e2a2d323d586dd800d1
zeus <136583387+zeusssz@users.noreply.github.com>
2024-08-25 12:32:33 +0530

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,28 @@
+import discord
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+
+model_name = "microsoft/DialoGPT-medium"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!chat'):
+        user_input = message.content[len('!chat '):]
+        input_ids = tokenizer.encode(user_input + tokenizer.eos_token, return_tensors='pt')
+        chat_history_ids = model.generate(input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+        bot_response = tokenizer.decode(chat_history_ids[:, input_ids.shape[-1]:][0], skip_special_tokens=True)
+        await message.channel.send(bot_response)
+
+token = os.environ['TOKEN']
+client.run(token)
