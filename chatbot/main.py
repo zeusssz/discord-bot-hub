4b57695b81c7ae176ba02af3a55b5624d21eaed6
zeus <136583387+zeusssz@users.noreply.github.com>
2024-09-02 17:26:02 +0530

    improvements

--- chatbot/main.py
+++ chatbot/main.py
@@ -1,31 +1,31 @@
 import discord
+from discord.app_commands import CommandTree
 from transformers import AutoModelForCausalLM, AutoTokenizer
-import torch
+import os
 from dotenv import load_dotenv
+
+load_dotenv()
 
 model_name = "microsoft/DialoGPT-medium"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name)
 
-load_dotenv()
-
-client = discord.Client()
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+tree = CommandTree(client)
 
 @client.event
 async def on_ready():
+    await tree.sync()
     print(f'We have logged in as {client.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('!chat'):
-        user_input = message.content[len('!chat '):]
-        input_ids = tokenizer.encode(user_input + tokenizer.eos_token, return_tensors='pt')
-        chat_history_ids = model.generate(input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
-        bot_response = tokenizer.decode(chat_history_ids[:, input_ids.shape[-1]:][0], skip_special_tokens=True)
-        await message.channel.send(bot_response)
-
-token = os.environ['CHAT-TOKEN']
+@tree.command(name="chat", description="Chat with the bot.")
+async def chat(interaction: discord.Interaction, message: str):
+    input_ids = tokenizer.encode(message + tokenizer.eos_token, return_tensors='pt')
+    chat_history_ids = model.generate(input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+    bot_response = tokenizer.decode(chat_history_ids[:, input_ids.shape[-1]:][0], skip_special_tokens=True)
+    await interaction.response.send_message(bot_response)
+    
+token = os.getenv('CHAT-TOKEN')
 client.run(token)
